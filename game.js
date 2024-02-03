c68cd49f6f78f4a3b5e5b434bbf4fe4bc4bd1a57
Jacob J <jacobjulagay@gmail.com>
2024-02-03 15:39:12 -0800

    adding singleround()

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ function startGame() {
         alert("See you next time!");
         return;
     }
-}
+};
 
 function getPLayerChoice() {
     let playerSelection = prompt("Rock, Paper, or Scissors?");
@@ -19,14 +19,13 @@ function getPLayerChoice() {
             playerSelection !== "paper" ||
             playerSelection !== "scissors") {
                 return playerSelection;
-            }
-        else {
+        } else {
             // This won't show because prompt() will show up before console
             alert("PLease try again, enter Rock, Paper, or Scissors.");
             console.log("PLease try again, enter Rock, Paper, or Scissors.");
         }
     }
-}
+};
 
 // Random choice of rock, paper, scissors for computerSelection
 function getComputerChoice() {
@@ -38,8 +37,27 @@ function getComputerChoice() {
     } else {
         return "scissors";
     }
+};
+
+function singleRound(playerSelection, computerSelection) {
+    //Draw
+    if (playerSelection === "rock" && computerSelection === "rock" ||
+        playerSelection === "paper" && computerSelection === "paper" ||
+        playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log("It's a draw");
+        return 0;
+    }
+    // Computer Win
+    else if (playerSelection === "scissors" && computerSelection === "rock" ||
+        playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "scissors") {
+        console.log(`You lose. ${playerSelection} loses to {computerSelection}`);
+        return 1;
+    }
+    // Player Win
+    else {  
+        console.log(`You lose. ${playerSelection} loses to {computerSelection}`);
+        return 2;
+    }
 }
-
-
-
 
