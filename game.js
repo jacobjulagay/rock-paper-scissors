7e11025f849ffce9d3c19ae451a73877c548acf1
Jacob J <jacobjulagay@gmail.com>
2024-02-03 11:31:28 -0800

    adding getPlayerChoice()

--- game.js
+++ game.js
@@ -7,3 +7,26 @@ function startGame() {
         return;
     }
 }
+
+function getPLayerChoice() {
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+    playerSelection = playerSelection.trim().toLowerCase();
+
+    let isPlayerSelectionValid = false;
+
+    while (isPlayerSelectionValid === false) {
+        if (playerSelection !== "rock" ||
+            playerSelection !== "paper" ||
+            playerSelection !== "scissors") {
+                return playerSelection;
+            }
+        else {
+            // This won't show because prompt() will show up before console
+            alert("PLease try again, enter Rock, Paper, or Scissors.")
+            console.log("PLease try again, enter Rock, Paper, or Scissors.")
+        }
+    
+    }
+
+
+}
